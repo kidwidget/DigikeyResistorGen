40c34ee09f40f1cf95bf6230770aaca3f1d8af43
kidwidget <kidwidget@gmail.com>
2024-08-25 00:43:24 -0400

    started the code

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
-""" import csv
+import sys
+import jinja2
+import pandas as pd
+import re
+import requests
 
+# Take the name of the CSV file as the first command line argument
+# Take the name of the output file as the second command line argument
 
+# omega symbol
+om = "\u03A9"
 
-# Open the CSV file and read its contents
-with open('data.csv', newline='') as csvfile:
-    # Create a DictReader object with the specified delimiter and quote character
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+if len(sys.argv) < 3:
+    print("Did you forget to type the input and/or output file names?")
+    sys.exit(1)
+
+dataFilePath = sys.argv[1]
+symbolOutputPath = sys.argv[2]
+
+# Open the data file
+df = pd.read_csv(dataFilePath, delimiter = ',', quotechar = '"')
+
+# Parse the file and assign variables
+for index, row in df.iterrows():
+    datasheetUrl =  df.loc[index, 'Datasheet']
+    if not re.match('http', datasheetUrl):
+        datasheetUrl = "FUBAR"
+
+    # check for multiple part numbers, take CT if available   
+    dkPart = df.loc[index, 'DK Part #']
+    if ',' in dkPart:
+        # there are multiple part numbers, take cut tape
+        if 'CT-ND' in dkPart:
+            dkPart = re.search('.*CT-ND', dkPart).group(0)
     
-    # Convert reader to a list to access specific rows by index
-    rows = list(reader)
+    mfrPart = df.loc[index, 'Mfr Part #']
+    price = df.loc[index, 'Price']
+
+    resistance = df.loc[index, 'Resistance']
+    # turn the ohms into 
+    resistance = resistance.replace("Ohms", om)
+    tolerance = df.loc[index, 'Tolerance']
+    power = df.loc[index, 'Power (Watts)']
+    # use the fraction syntax
+    power = re.sub(r'[0-9]+\.[0-9]+W,\s*', '', power)
     
-    # Check if there are at least 4 rows (excluding the header)
-
-    for row in rows:
-        print (row['Description'])
-   """
-from jinja2 import Environment, FileSystemLoader
-
-# Set up the Jinja2 environment
-env = Environment(loader=FileSystemLoader('.'))
-
-# Load the template
-template = env.get_template('template.txt')
-
-# Define the values for the placeholders
-context = {
-    'place': 'magical forest',
-    'character': 'wizard',
-    'activity': 'cast spells'
-}
-
-# Render the template with the context
-rendered_text = template.render(context)
-
-# Print or save the rendered text
-print(rendered_text)
+    dimensions = df.loc[index, 'Size / Dimension']
